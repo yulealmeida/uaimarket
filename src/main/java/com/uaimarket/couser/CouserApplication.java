15c801349da93894e0c89eead6c97d2f31eba851
yulealmeida <yule.alme2@gmail.com>
2025-07-18 10:54:03 -0300

    Project created

---
+++ src/main/java/com/uaimarket/couser/CouserApplication.java
@@ -0,0 +1,13 @@
+package com.uaimarket.couser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uaimarket/couser/CouserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uaimarket.couser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
