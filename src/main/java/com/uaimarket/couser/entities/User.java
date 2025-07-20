d860de9d68a38878f8e7703d1684589c8862a394
yulealmeida <yule.alme2@gmail.com>
2025-07-20 13:16:47 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/uaimarket/couser/entities/User.java
+++ src/main/java/com/uaimarket/couser/entities/User.java
@@ -11,7 +11,7 @@ public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private String email;
     private String phone;
@@ -21,14 +21,13 @@ public class User implements Serializable {
     public User () {
     }
 
-    public User (long id, String name, String email, String phone, String password) {
+    public User (Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.password = password;
     }
-
 
     public long getId () {
         return id;
---
+++ src/main/java/com/uaimarket/couser/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.uaimarket.couser.repositories;
+
+import com.uaimarket.couser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
