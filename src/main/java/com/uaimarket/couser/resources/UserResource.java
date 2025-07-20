af537446e70c5e0afa4f2162c402e77308ca3081
yulealmeida <yule.alme2@gmail.com>
2025-07-20 15:47:07 -0300

    Service layer, component registration

--- src/main/java/com/uaimarket/couser/resources/UserResource.java
+++ src/main/java/com/uaimarket/couser/resources/UserResource.java
@@ -2,18 +2,35 @@ package com.uaimarket.couser.resources;
 
 
 import com.uaimarket.couser.entities.User;
+import com.uaimarket.couser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"Maria","maria@bol.com","999-999","1234567");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity <User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 
