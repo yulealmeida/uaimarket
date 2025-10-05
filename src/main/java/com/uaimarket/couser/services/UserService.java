13acf57179e1846d6bae77655ebf88f00a53096f
yulealmeida <yule.alme2@gmail.com>
2025-10-05 15:47:44 -0300

    Exception handling - update

--- src/main/java/com/uaimarket/couser/services/UserService.java
+++ src/main/java/com/uaimarket/couser/services/UserService.java
@@ -4,6 +4,7 @@ import com.uaimarket.couser.entities.User;
 import com.uaimarket.couser.repositories.UserRepository;
 import com.uaimarket.couser.services.exceptions.DatabaseExeption;
 import com.uaimarket.couser.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +46,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity,obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData (User entity, User obj) {
